13e2f0ff85feb00417e38eb6996fb310598947f4
dbechar <122977421+dbechar@users.noreply.github.com>
2023-01-30 12:38:06 +0100

    Update Design.py

--- code/Design.py
+++ code/Design.py
@@ -47,7 +47,7 @@ df_complete = pd.concat([df_Polymorphemes, df_Monomorphemes, df_Error], axis = 1
 #---- Save experimental design as .csv ----
 df_complete.to_csv("../experimental_design/design.csv", index = False)
 prefixes.to_csv("../experimental_design/prefixes.csv", index= False)
-roots.to_csv("../experimental_design/roots.csv", index= False)
+#roots.to_csv("../experimental_design/roots.csv", index= False)
 suffixes.to_csv("../experimental_design/suffixes.csv", index= False)
 
     
